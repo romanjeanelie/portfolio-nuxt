f9c6f2f80a67795b44bb142b1cf82977ce1cfe2e
roman7501 <64649303+roman7501@users.noreply.github.com>
2021-09-14 10:13:38 +0200

    test without update slider project

--- components/scene/comps/Main.js
+++ components/scene/comps/Main.js
@@ -323,7 +323,7 @@ export default class Main {
   updatePage() {
     if (this.routeName === 'projects-slug') {
       this.background.animateOut()
-      this.sliderProject.display(this.slug)
+      // this.sliderProject.display(this.slug)
     }
 
     if (this.routeName === 'projects') {
@@ -362,7 +362,7 @@ export default class Main {
     if (progress === 1) {
       emitter.emit('CANVAS:LOADED')
       this.canvasIsLoaded = true
-      // this.updatePage()
+      this.updatePage()
     }
   }
 
